c4a68a26edb5a76402ef2d93513f922d1b939556
André Almeida <andre_rdo@hotmail.com>
2021-02-14 14:54:09 -0300

    add theme colors

---
+++ src/components/Header/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  height: 200px;
+  background-color: ${props=> props.theme.lavander};
+`;
---
+++ src/config/theme.js
@@ -0,0 +1,7 @@
+export const lavander = "#D9F0FA";
+export const blueMarguerite = "#5B4CB6";
+export const mediumSpringGreen = "#01F7AD";
+export const goldenPoppy = "#E1CB00";
+export const venetianRed = "#C50606";
+export const tropicalRainForest = "#047E5A";
+export const jacksonsPurple = "#413877";
