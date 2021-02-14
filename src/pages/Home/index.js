d18d36bf6c39fc05c45c878e43e753106babeeac
André Almeida <andre_rdo@hotmail.com>
2021-02-14 13:36:20 -0300

    create-react-app with styled-components

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, H1 } from './styles';
+
+function Home() {
+  return (
+    <Container>
+      <H1>Home page</H1>
+    </Container>
+  )
+}
+
+export default Home;
